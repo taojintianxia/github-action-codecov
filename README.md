8d4669f00de30312e378a1fc36de1d02fd94499d
孙念君 Nianjun Sun <sunnianjun@sphere-ex.com>
2022-09-02 16:07:28 +0800

    Add : add the designed uml for container

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# github-action-codecov
+# PlantUML Diagram
+
+## ShardingSphere
+  - IT Refactor
+
--- src/main/java/com/github/taojintianxia/actionexample/jacocotrigger/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-package com.github.taojintianxia.actionexample.jacocotrigger;
-
-public class HelloWorld {
-    
-    public String sayHello() {
-        System.out.println(" hello world !");
-        return "hello";
-    }
-}
